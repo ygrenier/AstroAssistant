776b71e3241d13a4c5f3d2dc2f0b91d4102ac690
Yan Grenier <ygrenier@ygrenier.com>
2015-05-31 21:18:25 +0200

    Création d'une librairie de gestion astrologique.

---
+++ sources/Framework/Astro.Library/AstroEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Astro.Library
+{
+    /// <summary>
+    /// Astrology engine
+    /// </summary>
+    public class AstroEngine
+    {
+    }
+}
---
+++ sources/Framework/Astro.Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Astro.Library")]
+[assembly: AssemblyDescription("Astrology Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Yan grenier")]
+[assembly: AssemblyProduct("AstroAssistant")]
+[assembly: AssemblyCopyright("Copyright © Yan Grenier 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("fr")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou utiliser par défaut les numéros de build et de version 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
